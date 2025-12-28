1ecd29864a7a6809e7f29bffba8d9fd5e013d04b
Ajay Pawar <ajaypawarexist@gmail.com>
2025-12-28 10:25:04 +0530

    updated

--- app/about/metadata.ts
+++
@@ -1,6 +0,0 @@
-﻿import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'About Us - Visions.services',
-  description: 'Learn about Visions.services, a leading software development company.',
-};
